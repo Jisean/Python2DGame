39754ca9bf243496c5450ab173a6f7f290fc0ed4
Nia <hsp0522@naver.com>
2016-10-16 23:58:30 +0900

    점프 구현.

--- Client/Player.py
+++ Client/Player.py
@@ -12,9 +12,11 @@ name = "Player"
 
 class Player:
     image = None
-    (LEFT_DOWN, RIGHT_DOWN, LEFT_UPATT, RIGHT_UPATT,
-     LEFT_UP, RIGHT_UP, LEFT_ATT, RIGHT_ATT, LEFT_RUN,
-     RIGHT_RUN, LEFT_STAND, RIGHT_STAND) = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+    (LEFT_FALL,RIGHT_FALL,
+     LEFT_JUMP, RIGHT_JUMP, LEFT_DOWN, RIGHT_DOWN,
+     LEFT_UPATT, RIGHT_UPATT,LEFT_UP, RIGHT_UP,
+     LEFT_ATT, RIGHT_ATT, LEFT_RUN,
+     RIGHT_RUN, LEFT_STAND, RIGHT_STAND) = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
 
     MOVE = None
     UPSIDE = None
@@ -22,12 +24,15 @@ class Player:
     ATT = None
     POSLEFT = None
     POSRIGHT = None
+    PUSHLEFT = None
+    PUSHRIGHT = None
     JUMP = None
     FALLING = None
 
 
     def __init__(self):
         self.x, self.y = random.randint(100, 700), 90
+        self.jumpacc = 5
         self.frame = random.randint(0, 27)
         self.POSRIGHT = True
         self.MOVE = False
@@ -38,19 +43,27 @@ class Player:
     def handle_inputs(self,event):
         if(event.type, event.key) == (SDL_KEYDOWN, SDLK_LEFT):
             self.MOVE = True
+            self.PUSHLEFT = True
+            self.PUSHRIGHT = False
             self.POSLEFT = True
             self.POSRIGHT = False
                 # 왼이동
         elif(event.type, event.key) == (SDL_KEYDOWN, SDLK_RIGHT):
             self.MOVE = True
+            self.PUSHLEFT = False
+            self.PUSHRIGHT = True
             self.POSLEFT = False
             self.POSRIGHT = True
                 #오른이동
         elif(event.type, event.key) == (SDL_KEYUP, SDLK_LEFT):
             self.MOVE = False
+            self.PUSHLEFT = False
+            self.PUSHRIGHT = False
                 #왼이동후 정지
         elif (event.type, event.key) == (SDL_KEYUP, SDLK_RIGHT):
             self.MOVE = False
+            self.PUSHLEFT = False
+            self.PUSHRIGHT = False
                 #오른이동후 정지
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_z):
              self.ATT = True
@@ -70,6 +83,10 @@ class Player:
         elif (event.type, event.key) == (SDL_KEYUP, SDLK_DOWN):
             self.DOWNSIDE = False
                 #숙이기 정지
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE) :
+            self.JUMP = True
+            self.frame = 0;
+                #점프
 
     def states(self):
         if self.MOVE == True:
@@ -105,6 +122,21 @@ class Player:
                 elif self.UPSIDE == False:
                     self.state = self.RIGHT_ATT
 
+        if self.JUMP == True:
+            if self.POSLEFT == True:
+                self.state = self.LEFT_JUMP
+            elif self.POSRIGHT == True:
+                self.state = self.RIGHT_JUMP
+
+            if self.frame > 7 :
+                self.JUMP = False
+                self.FALLING = True
+        if self.FALLING == True:
+            if self.POSLEFT == True:
+                self.state = self.LEFT_FALL
+            elif self.POSRIGHT == True:
+                self.state = self.RIGHT_FALL
+
     def frames(self):
         if self.state == self.RIGHT_STAND or self.state == self.LEFT_STAND:
             self.frame = (self.frame + 1) % 27
@@ -118,10 +150,31 @@ class Player:
             self.frame = (self.frame + 1) % 2
         elif self.state == self.LEFT_UP or self.state == self.RIGHT_UP or self.state == self.LEFT_DOWN or self.state == self.RIGHT_DOWN:
             self.frame = (self.frame + 1) % 1
+        elif self.state == self.LEFT_JUMP or self.state == self.RIGHT_JUMP:
+            self.frame = (self.frame + 1) % 9
+            self.jumpacc = self.jumpacc + 2
+            self.y = self.y + self.jumpacc
+            if self.PUSHRIGHT == True:
+                self.x = min(800, self.x + 10)
+            elif self.PUSHLEFT == True:
+                self.x = max(0, self.x - 10)
+
+        elif self.state == self.LEFT_FALL or self.state == self.RIGHT_FALL:
+            self.frame = (self.frame + 1) % 3
+            self.y = max(90, self.y - self.jumpacc)
+            if self.PUSHRIGHT == True:
+                self.x = min(800, self.x + 10)
+            elif self.PUSHLEFT == True:
+                self.x = max(0, self.x - 10)
+
 
 
     def update(self):
         self.states()
+        if self.y == 90:
+            self.FALLING = False
+            self.jumpacc = 5
+        print(self.jumpacc)
         self.frames()
         delay(0.01)
 
